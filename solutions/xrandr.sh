ef9dd33d0dd483a0d9e7be9f913f02c60961fda3
Sergei Shilovsky <sshilovsky@gmail.com>
2013-10-27 08:51:12 +0400

    xrandr turns off VGA1 when disconnected

--- solutions/xrandr.sh
+++ solutions/xrandr.sh
@@ -7,10 +7,12 @@ if [ "$lvds" = "connected" ]
 then
     if [ "$vga" = "connected" ]
     then
-        xrandr --auto --output VGA1 --above LVDS1
+        xrandr --auto --output VGA1 --left-of LVDS1
         xrandr --output VGA1 --mode 1280x1024 -s 1280x1024
         xrandr --output LVDS1 --mode 1024x600 -s 1024x600
     else
-        xrandr --auto --output VGA1
+        xrandr --output VGA1 --off
+        xrandr --output LVDS1 --auto
+        xrandr --output LVDS1 --mode 1024x600 -s 1024x600
     fi
 fi
