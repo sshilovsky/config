8fe9d5962a71bc0065103a3f6858bf58975e1e10
Sergei Shilovsky <sshilovsky@gmail.com>
2013-11-17 05:27:17 +0400

    vim: ghmod-vim installed (haskell)

--- .vim/bundle/README.sh
+++ .vim/bundle/README.sh
@@ -1,7 +1,17 @@
-1. Most of these repos should be converted to submodules
+Most of these bundles should be converted to submodules
+But some of them are already converted.
 
-2. neco-ghc is.
+1. neco-ghc is.
     DEP: ghc-mod : debian (warn: emacs)
     URL: https://github.com/eagletmt/neco-ghc
+
+2. ghcmod-vim is.
+    DEP: ghc-mod 2.1.2 : cabal [ happy : debian ]
+    DEP: vimproc.vim : bundle
+    URL: https://github.com/eagletmt/ghcmod-vim
+
+3. vimproc.vim is.
+    URL: https://github.com/Shougo/vimproc.vim
+    ACT: have to be compiled with its Makefile
 
 # vim: ft=
