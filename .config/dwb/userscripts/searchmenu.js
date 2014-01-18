560ad4131135aeba9791df02c64e2b92e2efce8b
Sergei Shilovsky <sshilovsky@gmail.com>
2014-01-18 19:51:52 +0400

    dwb: searchmenu.js: workaround text selection issue for github.com

--- .config/dwb/userscripts/searchmenu.js
+++ .config/dwb/userscripts/searchmenu.js
@@ -19,18 +19,31 @@ function read_engines()
     });
 }
 
+function getSelection() 
+// TODO issue: doesn't work for github.com on some dwb builds
+{
+    var frames = tabs.current.allFrames;
+    for (var i=frames.length-1; i>=0; --i) 
+    {
+        var selection = JSON.parse(frames[i].inject("return document.getSelection().toString()"));
+        if (selection && selection.length > 0)
+            return selection;
+    }
+    return null;
+}
+
 read_engines()
 
 Signal.connect("contextMenu", function (wv, menu) {
     if(!search_engines) return;
 
-    var text = (util.getSelection() || "").trim();
+    var text = (getSelection() || "").trim();
     if (text.length == 0) return;
 
     menu.addItems([null])
     search_engines.forEach(function(entry) {
         menu.addItems([{   
-            label: "Search with "+entry.host,
+            label: "Search \""+text+"\" with "+entry.host,
             callback: function() {
                 execute("tabopen "+entry.shortcut+" "+text);
             }
