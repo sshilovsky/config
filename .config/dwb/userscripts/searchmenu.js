c893e961db79d24dcfc8e1aaac286e2eaa5c1c69
Sergei Shilovsky <sshilovsky@gmail.com>
2014-01-18 18:01:53 +0400

    dwb: searchmenu.js introduced

---
+++ .config/dwb/userscripts/searchmenu.js
@@ -0,0 +1,41 @@
+//!javascript
+
+var search_engines = null
+
+function read_engines()
+{
+    search_engines = io.read(data.searchEngines).split("\n").filter(function(line) { 
+        return line.trim().length > 0; 
+    }).map(function(line) {
+        var token = line.split(" ");
+        var shortcut = token[0];
+        var url = token.slice(1).join(" ");
+        var parsedUrl = net.parseUri(url);
+        return {
+            shortcut : shortcut, 
+            url : url, 
+            host : (parsedUrl ? parsedUrl.host : null)
+        };
+    });
+}
+
+read_engines()
+
+Signal.connect("contextMenu", function (wv, menu) {
+    if(!search_engines) return;
+
+    var text = (util.getSelection() || "").trim();
+    if (text.length == 0) return;
+
+    menu.addItems([null])
+    search_engines.forEach(function(entry) {
+        menu.addItems([{   
+            label: "Search with "+entry.host,
+            callback: function() {
+                io.print(entry.shortcut);
+                execute("tabopen "+entry.shortcut+" "+text);
+            }
+        }])
+    })
+})
+
