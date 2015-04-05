2b56a0f2a15dbe4687522c0b1fbce2b62a992249
Sergei Shilovsky <sshilovsky@gmail.com>
2015-04-05 10:55:52 +0300

    fix switch-mode.sh to use bash instead of sh

--- bin/switch-mode.sh
+++ bin/switch-mode.sh
@@ -1,23 +1,27 @@
-#!/bin/sh
+#!/bin/bash
+
+SQUARE="VGA-0"
+WIDE="DVI-I-1"
+mode=2
 
 function switch()
 {
     case $mode in
         0)
             mode=1
-            xrandr --output VGA1 --mode 1280x1024 --size 1280x1024
-            xrandr --output HDMI1 --off
+            xrandr --output $SQUARE --mode 1280x1024 --size 1280x1024
+            xrandr --output $WIDE --off
             ;;
         1)
             mode=2
-            xrandr --output HDMI1 --mode 1920x1080 --size 1920x1080
-            xrandr --output VGA1 --off
+            xrandr --output $WIDE --mode 1920x1080 --size 1920x1080
+            xrandr --output $SQUARE --off
             ;;
         *)
             mode=0
-            xrandr --output VGA1 --mode 1280x1024 --size 1280x1024
-            xrandr --output HDMI1 --mode 1920x1080 --size 1920x1080
-            xrandr --auto --output HDMI1 --right-of VGA1
+            xrandr --output $SQUARE --mode 1280x1024 --size 1280x1024
+            xrandr --output $WIDE --mode 1920x1080 --size 1920x1080
+            xrandr --auto --output $WIDE --right-of $SQUARE
             ;;
     esac
 }
